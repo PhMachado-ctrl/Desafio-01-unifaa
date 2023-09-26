2510f28449dc7cbee7f1b93a435574b8ad2fe5d4
Pedro Henrique <phmachado555@gmail.com>
2023-09-25 23:46:50 -0300

    email and password js

---
+++ js/login.js
@@ -0,0 +1,13 @@
+// Catch 3 values from screen.
+let email = document.getElementById('email');
+let senha = document.getElementById('password');
+let btnEntrar = document.getElementById('btn-enter');
+
+// Take the Button Click Event
+btnEntrar.addEventListener('click', () => {
+    // take the email typed
+    let userEmail = email.value;
+    // take the password  typed
+    let userSenha = senha.value;
+
+})
